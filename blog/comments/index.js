3e556927cb381b69f1cb2f605a1c51c27198a94e
Wajdzik, Maciej <wajdzik.maciej@gmail.com>
2020-05-09 16:30:08 +0200

    course-microservices-nodejs blog project simple services added

---
+++ blog/comments/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const {randomBytes} = require("crypto");
+
+const app = express();
+app.use(bodyParser.json());
+
+const commentsByPostId = {};
+
+app.get('/posts/:id/comments', (req, res) => {
+    const postId = req.params.id;
+    res.send(commentsByPostId[postId]);
+});
+
+app.post('/posts/:id/comments', (req, res) => {
+    const postId = req.params.id;
+    const id = randomBytes(4).toString('hex');
+    const {content} = req.body;
+    const comments = commentsByPostId[postId] || [];
+    comments.push({id, content});
+    commentsByPostId[postId] = comments;
+    res.status(201).send(comments);
+});
+
+app.listen(4001, () => {
+    console.log("Comments Service listening...");
+});
