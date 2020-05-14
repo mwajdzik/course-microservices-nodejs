571887cbaba80db4c3c90509558d62b33fccda20
Wajdzik, Maciej <wajdzik.maciej@gmail.com>
2020-05-14 13:28:33 +0200

    course-microservices-nodejs blog project ReactJS changes

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -1,9 +1,11 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const {randomBytes} = require("crypto");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const {randomBytes} = require('crypto');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const posts = {};
 
@@ -19,5 +21,5 @@ app.post('/posts', (req, res) => {
 });
 
 app.listen(4000, () => {
-    console.log("Posts Service listening...");
+    console.log('Posts Service listening...');
 });
