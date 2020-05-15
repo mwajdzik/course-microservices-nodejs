28702b1df551879fed332e5d1727fddc2b6eb353
Wajdzik, Maciej <wajdzik.maciej@gmail.com>
2020-05-15 19:47:21 +0200

    course-microservices-nodejs blog project save all events so they can be used when query service starts up

--- blog/query/index.js
+++ blog/query/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const axios = require('axios');
 const cors = require('cors');
 
 const app = express();
@@ -8,15 +9,7 @@ app.use(cors());
 
 const posts = {};
 
-app.get('/posts', (req, res) => {
-    res.send(posts);
-});
-
-app.post('/events', (req, res) => {
-    console.log('Received Event', req.body);
-
-    const {type, data} = req.body;
-
+function handleEvent(type, data) {
     if (type === 'PostCreated') {
         const {id, title} = data;
         posts[id] = {id, title, comments: []};
@@ -35,10 +28,29 @@ app.post('/events', (req, res) => {
         comment.status = status;
         comment.content = content;
     }
+}
+
+app.get('/posts', (req, res) => {
+    res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+    console.log('Received Event', req.body);
+
+    const {type, data} = req.body;
+    handleEvent(type, data)
 
     res.send({});
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
     console.log('Query Service listening...');
+
+    const res = await axios.get('http://localhost:4005/events');
+
+    for (let event of res.data) {
+        const {type, data} = event;
+        console.log('Processing event:', type)
+        handleEvent(type, data);
+    }
 });
