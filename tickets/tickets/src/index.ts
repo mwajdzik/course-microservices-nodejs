cb430a84100d146fe6bd31de95c10aef6ce60e92
Wajdzik, Maciej <wajdzik.maciej@gmail.com>
2020-06-07 14:40:44 +0200

    course-microservices-nodejs blog project add envs

--- tickets/tickets/src/index.ts
+++ tickets/tickets/src/index.ts
@@ -2,18 +2,19 @@ import mongoose from 'mongoose';
 import {app} from './app';
 import {natsWrapper} from "./nats-wrapper";
 
-const start = async () => {
-    if (!process.env.JWT_KEY) {
-        throw new Error('JWT_KEY must be defined');
+const ensureEnv = (name: string) => {
+    if (!process.env[name]) {
+        throw new Error(`${name} must be defined`);
     }
+}
 
-    if (!process.env.MONGO_URI) {
-        throw new Error('MONGO_URI must be defined');
-    }
+const start = async () => {
+    ['JWT_KEY', 'MONGO_URI', 'NATS_CLUSTER_ID', 'NATS_CLIENT_ID', 'NATS_URL']
+        .forEach(ensureEnv);
 
     try {
         // ticketing - clusterId used in tickets/infra/k8s/nats-depl.yaml
-        await natsWrapper.connect('ticketing', '12345', 'http://nats-srv:4222');
+        await natsWrapper.connect(process.env.NATS_CLUSTER_ID!, process.env.NATS_CLIENT_ID!, process.env.NATS_URL!);
 
         natsWrapper.client.on('close', () => {
             console.log('NATS connection closed!');
@@ -23,7 +24,7 @@ const start = async () => {
         process.on('SIGINT', () => natsWrapper.client.close());
         process.on('SIGTERM', () => natsWrapper.client.close());
 
-        await mongoose.connect(process.env.MONGO_URI, {
+        await mongoose.connect(process.env.MONGO_URI!, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true
